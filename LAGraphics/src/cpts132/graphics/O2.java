99a1644076f6d931e8e0f95be7213addb5684f09
Stacy <Stacy@DESKTOP-50AU5UD>
2017-02-27 11:24:16 -0800

    Final

---
+++ LAGraphics/src/cpts132/graphics/O2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpts132.graphics;
+
+/**
+ *
+ * @author Stacy
+ */
+public class O2 {
+    public void lineColor(java.awt.Color rgb){
+        
+    }
+    
+    public void fillColor(java.awt.Color rgb){
+        
+    }
+    
+}
