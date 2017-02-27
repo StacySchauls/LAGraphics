2ea1046a96cac0170e760a92aa79d16c250f64ec
Stacy <Stacy@DESKTOP-50AU5UD>
2017-02-27 12:02:17 -0800

    Final

--- LAGraphics/src/cpts132/graphics/O2.java
+++ LAGraphics/src/cpts132/graphics/O2.java
@@ -9,13 +9,48 @@ package cpts132.graphics;
  *
  * @author Stacy
  */
-public class O2 {
-    public void lineColor(java.awt.Color rgb){
+public class O2 extends javax.swing.JComponent{
+    private java.awt.Color lc;
+    private java.awt.Color fc;
+    /**
+     * 
+     */
+     public O2() {
+    setPreferredSize(new java.awt.Dimension(25, 25));
+    this.setLineColor(java.awt.Color.BLACK);
+    this.setFillColor(java.awt.Color.WHITE);
+  }
+     /**
+      * 
+      * @param g 
+      */
+     public void paintComponent(java.awt.Graphics g) {
+        // paint the background
+        super.paintComponent(g);
+        g.setColor(fc);
+        g.fillOval(0, 0, getWidth()-1, getHeight()-1);
+        g.setColor(lc);
+        g.drawOval(0, 0, getWidth()-1, getHeight()-1);
+     }
+    
+    /**
+     * 
+     * @param rgb 
+     */
+    public void setLineColor(java.awt.Color rgb){
+        lc = rgb;
+        repaint();
+    }
+    
+    /**
+     * 
+     * @param rgb 
+     */
+    public void setFillColor(java.awt.Color rgb){
+        fc = rgb;
+        repaint();
         
     }
     
-    public void fillColor(java.awt.Color rgb){
-        
-    }
     
 }
